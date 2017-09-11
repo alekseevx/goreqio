bf1063d40b55f3280f1a72e58d1bfa5010e795fe
AlekseevAn <aleksey_alekseev@relex.ru>
2017-09-11 14:17:09 +0300

    Added doTestHTTP

--- goreqio.go
+++ goreqio.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"math/rand"
 	"net"
+	"net/http"
 	"os"
 	"path/filepath"
 	"strconv"
@@ -39,8 +40,12 @@ func parseArgs() programOpts {
 		servers:        args[1:]}
 }
 
-func doTest(servers []string, reqCounter *uint64, connCount *int32) {
-	req := []byte("GET /hello HTTP/1.1\r\n")
+func doTestTCP(servers []string, reqCounter *uint64, connCount *int32) {
+	req := []byte("GET /hello HTTP/1.1\r\n" +
+		"Host: 172.17.0.129\r\n" +
+		"Content-Type: text/plain\r\n" +
+		"Connection: Close\r\n" +
+		"\r\n")
 
 	s := rand.NewSource(time.Now().Unix())
 	r := rand.New(s)
@@ -53,6 +58,7 @@ func doTest(servers []string, reqCounter *uint64, connCount *int32) {
 			fmt.Printf("Can't connect to %v: %v\n", server, err)
 			continue
 		}
+		defer conn.Close()
 		atomic.AddInt32(connCount, 1)
 
 		_, err = conn.Write(req)
@@ -65,6 +71,7 @@ func doTest(servers []string, reqCounter *uint64, connCount *int32) {
 					if err != io.EOF {
 						fmt.Printf("Can't receive resp from %v: %v\n", server, err)
 					} else {
+						// fmt.Printf("%v\n", string(readBuf))
 						atomic.AddUint64(reqCounter, 1)
 					}
 
@@ -73,8 +80,52 @@ func doTest(servers []string, reqCounter *uint64, connCount *int32) {
 			}
 		}
 
-		conn.Close()
 		atomic.AddInt32(connCount, -1)
+	}
+}
+
+func doTestHTTP(servers []string, reqCounter *uint64, connCount *int32) {
+	s := rand.NewSource(time.Now().Unix())
+	r := rand.New(s)
+
+	readBuf := make([]byte, 2*1024)
+	client := &http.Client{}
+
+	var reqs []*http.Request
+	for i := 0; i < len(servers); i++ {
+		req, err := http.NewRequest("GET", "http://"+servers[i]+"/hello", nil)
+		req.Header.Add("Host", "172.17.0.129")
+		if err != nil {
+			fmt.Printf("Can't create HttpRequest: %v\n", err)
+			return
+		}
+		reqs = append(reqs, req)
+	}
+
+	for {
+		server := servers[r.Intn(len(servers))]
+		req := reqs[r.Intn(len(servers))]
+
+		resp, err := client.Do(req)
+		if err != nil {
+			fmt.Printf("client.Do failed: %v\n", err)
+			continue
+		}
+
+		defer resp.Body.Close()
+		for {
+			_, err := resp.Body.Read(readBuf)
+			if err != nil {
+				if err != io.EOF {
+					fmt.Printf("Can't receive resp from %v: %v\n", server, err)
+				} else {
+					// fmt.Printf("%v\n", string(readBuf))
+					atomic.AddUint64(reqCounter, 1)
+				}
+
+				break
+			}
+		}
 	}
 }
 
@@ -86,7 +137,7 @@ func main() {
 	var reqCounter uint64
 	var connCount int32
 	for i := 0; i < opts.goroutineCount; i++ {
-		go doTest(opts.servers, &reqCounter, &connCount)
+		go doTestHTTP(opts.servers, &reqCounter, &connCount)
 	}
 
 	startTime := time.Now()
@@ -97,8 +148,8 @@ func main() {
 		stopTime := time.Now()
 
 		elapsed := stopTime.Sub(startTime)
-		fmt.Printf("RPS=%v", float64(stopReqCounter-startReqCounter)/elapsed.Seconds())
-		fmt.Printf("ConnCount=%v", connCount)
+		fmt.Printf("RPS=%v\n", float64(stopReqCounter-startReqCounter)/elapsed.Seconds())
+		fmt.Printf("ConnCount=%v\n", connCount)
 
 		startTime, startReqCounter = stopTime, stopReqCounter
 	}
